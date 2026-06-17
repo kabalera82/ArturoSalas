740cb71baa0268940079d82b3f5898fd6894c6df
kabalera82 <kabalera@gmail.com>
2026-06-17 15:12:44 +0200

    style: paleta negro/blanco/rojo y estilos globales

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,23 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { PageLayout } from './layouts/PageLayout'
-import { HomePage } from './pages/HomePage'
-import { AuthProvider } from './context/AuthContext'
+import { HomePage } from './pages/home/HomePage'
+import { ShopPage } from './pages/shop/ShopPage'
+import { AuthProvider } from './context/AuthProvider'
+import { CartProvider } from './context/CartProvider'
 
 export const App = () => {
   return (
     <AuthProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<PageLayout />}>
-            <Route index element={<HomePage />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <CartProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<PageLayout />}>
+              <Route index element={<HomePage />} />
+              <Route path="/shop" element={<ShopPage />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </CartProvider>
     </AuthProvider>
   )
 }
