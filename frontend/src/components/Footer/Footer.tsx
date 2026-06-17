b8364e4cc1e2b91aca4af06e27124f45ab915de4
kabalera82 <kabalera@gmail.com>
2026-06-17 15:12:55 +0200

    refactor(Footer): activeHref dinámico y estilos alineados con navbar

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -1,23 +1,26 @@
 import './Footer.css';
+import { useLocation } from 'react-router-dom';
 import { NavLinks } from '../shared/NavLinks/NavLinks';
-import { navItems } from '../NavBar/navItems';
+import { navItems } from '../../data/navItems';
 import { footItems } from './footItems';
 
 export const Footer = () => {
-    return (
-        <footer className='footer'>
-            <ul className='foot_logos'>
-                {footItems.map((item) => (
-                    <li className='footlinks_item' key={item.href}>
-                        <a className='footlinks_link' href={item.href} target="_blank" rel="noopener noreferrer">
-                            <img src={item.imgSrc} alt={item.altText} />
-                        </a>
-                    </li>
-                ))}
-            </ul>
-            <div className='foot_links'>
-                <NavLinks items={navItems} activeHref='#inicio' onLinkClick={() => {}} />
-            </div>
-        </footer>
-    );
-}
+  const { pathname } = useLocation();
+
+  return (
+    <footer className='footer'>
+      <ul className='foot_logos'>
+        {footItems.map((item) => (
+          <li className='footlinks_item' key={item.href}>
+            <a className='footlinks_link' href={item.href} target='_blank' rel='noopener noreferrer'>
+              <img src={item.imgSrc} alt={item.altText} />
+            </a>
+          </li>
+        ))}
+      </ul>
+      <div className='foot_links'>
+        <NavLinks items={navItems} activeHref={pathname} />
+      </div>
+    </footer>
+  );
+};
